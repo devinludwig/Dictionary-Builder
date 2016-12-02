515af910e8e054c85ab55ce4f999d5ba80b0edcb
Devin Ludwig <devinludwig@gmail.com>
2016-12-02 13:34:53 -0800

    add third passing integration spec.

--- spec/dictionary_integration_spec.rb
+++ spec/dictionary_integration_spec.rb
@@ -20,3 +20,11 @@ describe('the word page path', {:type => :feature}) do
     expect(page).to have_content('New Definition')
   end
 end
+
+describe('the new definition path', {:type => :feature}) do
+  it('displays the definition form page when user clicks the new definition button') do
+    visit('/word/1')
+    click_link('New Definition')
+    expect(page).to have_content('Part of Speech')
+  end
+end
