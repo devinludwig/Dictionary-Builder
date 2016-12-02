e2ddf3b40a81d04287805955818091615d455026
Devin Ludwig <devinludwig@gmail.com>
2016-12-02 12:03:45 -0800

    add routing to app.rb file including link to add definitions to a word page via a new definition.erb view.

---
+++ app.rb
@@ -0,0 +1,35 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('./lib/definition')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all()
+  erb(:index)
+end
+
+post('/new_word') do
+  word = params.fetch('word')
+  Word.new({:spelling => word}).save()
+  @words = Word.all()
+  erb(:index)
+end
+
+get('/word/:id') do
+  @word = Word.find(params.fetch('id').to_i)
+  erb(:word)
+end
+
+get('/word/:id/definition') do
+  @word = Word.find(params.fetch('id').to_i)
+  erb(:definition)
+end
+
+post('/new_definition') do
+  word_id = params.fetch('word_id')
+  @word = Word.find(word_id.to_i())
+  definition = params.fetch('definition')
+  Definition.new({:text => definition}).save_definition()
+  erb(:word)
+end
--- lib/word.rb
+++ lib/word.rb
@@ -24,5 +24,13 @@ class Word
     @@words = []
   end
 
-
+  define_singleton_method(:find) do |id|
+    found_word = nil
+    @@words.each() do |word|
+      if word.id().eql?(id)
+        found_word = word
+      end
+    end
+    found_word
+  end
 end
